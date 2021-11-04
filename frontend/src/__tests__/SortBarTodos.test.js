18fa9b730617999137d4adfb161e22bbbcbdb732
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-11-04 18:10:06 -0300

    feat: sortbartodos tests

--- frontend/src/__tests__/SortBarTodos.test.js
+++ frontend/src/__tests__/SortBarTodos.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import SortBarTodos from '../components/todos/SortBarTodos';
 import { renderWithRouterAndRedux } from '../services/renderWithRouterAndRedux';
 
@@ -20,8 +20,6 @@ const sortDesBtnId = 'sort-des-btn';
 const sortAscIconId = 'sort-asc-icon';
 const sortDesIconId = 'sort-des-icon';
 
-jest.mock('axios');
-
 describe('SortBarTodos component', () => {
   it('renders all inputs and buttons', () => {
     renderWithRouterAndRedux(
@@ -39,5 +37,21 @@ describe('SortBarTodos component', () => {
     expect(sortDesBtn).toBeInTheDocument();
     expect(sortAscIcon).toBeInTheDocument();
     expect(sortDesIcon).toBeInTheDocument();
+  });
+
+  it('changes order attribute in state todos after clicking on sorting buttons', () => {
+    const { store } = renderWithRouterAndRedux(
+      <SortBarTodos />, { route: '/' }, initialState,
+    );
+
+    const sortDesBtn = screen.getByTestId(sortDesBtnId);
+    fireEvent.click(sortDesBtn);
+
+    expect(store.getState().todos.sort.order).toEqual('des');
+
+    const sortAscBtn = screen.getByTestId(sortAscBtnId);
+    fireEvent.click(sortAscBtn);
+
+    expect(store.getState().todos.sort.order).toEqual('asc');
   });
 });
